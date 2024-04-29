0e9809e310442c4b0361586a0621701d6dd8b6b3
Sttrano-Alexey <80354673+Sttrano-Alexey@users.noreply.github.com>
2024-04-29 18:02:45 +0300

    Update ProductList.jsx

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -18,7 +18,7 @@ const ProductList = () => {
   const [selectedCategories, setSelectedCategories] = useState([]); // выбранные категории
 
   useEffect(() => {
-    fetch("../src/DATA/products.json")
+    fetch("https://raw.githubusercontent.com/Sttrano-Alexey/bd/main/products.json")
       .then((res) => res.json())
       .then((products) => {
         // Получаем уникальные категории и бренды
